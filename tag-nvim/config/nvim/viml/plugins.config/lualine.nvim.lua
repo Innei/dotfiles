fd6f6cb529fac194c1290c926236c0f58d36fd1c
Innei <tukon479@gmail.com>
2022-09-30 18:00:41 +0800

    lualine
    Signed-off-by: Innei <tukon479@gmail.com>

--- tag-nvim/config/nvim/viml/plugins.config/lualine.nvim.lua
+++ tag-nvim/config/nvim/viml/plugins.config/lualine.nvim.lua
@@ -25,25 +25,25 @@ require('lualine').setup {
       --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
       -- or a function that returns a table as such:
       --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
-      sources = { 'nvim_diagnostic', 'coc' },
+      -- sources = { 'nvim_diagnostic', 'coc' },
 
-      -- Displays diagnostics for the defined severity types
-      sections = { 'error', 'warn', 'info', 'hint' },
+      -- -- Displays diagnostics for the defined severity types
+      -- sections = { 'error', 'warn', 'info', 'hint' },
 
-      diagnostics_color = {
-        -- Same values as the general color option can be used here.
-        error = 'DiagnosticError', -- Changes diagnostics' error color.
-        warn  = 'DiagnosticWarn', -- Changes diagnostics' warn color.
-        info  = 'DiagnosticInfo', -- Changes diagnostics' info color.
-        hint  = 'DiagnosticHint', -- Changes diagnostics' hint color.
-      },
-      symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
-      colored = true, -- Displays diagnostics status in color if set to true.
-      update_in_insert = false, -- Update diagnostics in insert mode.
-      always_visible = false, -- Show diagnostics even if there are none.
+      -- diagnostics_color = {
+      --   -- Same values as the general color option can be used here.
+      --   error = 'DiagnosticError', -- Changes diagnostics' error color.
+      --   warn  = 'DiagnosticWarn', -- Changes diagnostics' warn color.
+      --   info  = 'DiagnosticInfo', -- Changes diagnostics' info color.
+      --   hint  = 'DiagnosticHint', -- Changes diagnostics' hint color.
+      -- },
+      -- symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
+      -- colored = true, -- Displays diagnostics status in color if set to true.
+      -- update_in_insert = false, -- Update diagnostics in insert mode.
+      -- always_visible = false, -- Show diagnostics even if there are none.
     },
     lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_x = { 'encoding', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' }
   },
