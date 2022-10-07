179db3b4759b9738c77826501b0d8bfe7d63e05d
Innei <tukon479@gmail.com>
2022-10-07 22:21:39 +0800

    update
    Signed-off-by: Innei <tukon479@gmail.com>

--- tag-nvim/config/nvim/viml/plugins.config/indent-blankline.nvim.lua
+++ tag-nvim/config/nvim/viml/plugins.config/indent-blankline.nvim.lua
@@ -23,7 +23,7 @@ local options = {
   show_trailing_blankline_indent = false,
   show_first_indent_level = false,
   show_current_context = true,
-  show_current_context_start = true,
+  -- show_current_context_start = true,
 }
 
 blankline.setup(options)
