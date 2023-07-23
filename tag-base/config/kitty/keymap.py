46ccb99353743f00699dbe0c0eae0aac8ae6f423
Innei <i@innei.in>
2023-07-23 16:41:08 +0800

    update

--- tag-base/config/kitty/keymap.py
+++ tag-base/config/kitty/keymap.py
@@ -15,11 +15,11 @@ def handle_result(args, answer, target_window_id, boss):
     cmd = window.child.foreground_cmdline[0]
     if args[1] == "C-i":
         # Move cursor to the end of line, specific to zsh
-        if cmd[-3:] == "zsh":
+        if cmd.endswith("zsh"):
             window.write_to_child("\x1b[105;5u")
 
         # A workaround for tmux to fix its bug of Ctrl+i recognition, sending a Ctrl-; instead
-        elif cmd[-4:] == "tmux":
+        elif cmd.endswith("tmux"):
             window.write_to_child("\x1b[59;5u")
             return
 
@@ -31,29 +31,25 @@ def handle_result(args, answer, target_window_id, boss):
         else:
             window.write_to_child("\x09")
 
-    elif args[1] == "S-s":
-        if cmd[-4:] == "nvim":
+    elif cmd.endswith("nvim"):
+        if args[1] == "S-s":
             window.write_to_child("\x1b\x1b[115;5u")
-    elif args[1] == "M-f":
-        if cmd[-4:] == "nvim":
+        elif args[1] == "M-f":
             window.write_to_child('\x1b\x1b[102;9u')
-    elif args[1] == "C-p":
-        if cmd[-4:] == "nvim":
+        elif args[1] == "C-p":
             window.write_to_child('\x1b\x1b[112;5u')
-    elif args[1] == "C-d":
-        if cmd[-4:] == "nvim":
+        elif args[1] == "C-d":
             window.write_to_child('\x1b\x1b[100;5u')
-    elif args[1] == "M-x":
-        if cmd[-4:] == "nvim":
+        elif args[1] == "M-x":
             window.write_to_child('\x1b\x1b[120;3u')
-    elif args[1] == "M-c":
-        if cmd[-4:] == "nvim":
+        elif args[1] == "M-c":
             window.write_to_child('\x1b\x1b[99;3u')
-    elif args[1] == "M-z":
-        if cmd[-4:] == "nvim":
+        elif args[1] == "M-z":
             window.write_to_child('\x1b\x1b[122;3u')
-    elif args[1] == "M-p":
-        if cmd[-4:] == "nvim":
+        elif args[1] == "M-p":
             window.write_to_child('\x1b\x1b[112;3u')
-
+        elif args[1] == "M-backspace":
+            window.write_to_child('\x1b\x1b[127;3u')
+        elif args[1] == "M-.":
+            window.write_to_child('\x1b\x1b[46;9u')
 
